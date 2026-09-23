7ed8aa16436b693f551278ea67075c4794970556
Rizal <rizalabdurrahman877@gmail.com>
2026-09-23 08:24:14 +0700

    update links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ const links = [
   { name: "Projects", href: "#projects" },
   { name: "Experience", href: "#experience" },
   { name: "Contact", href: "#contact" },
+  {name : "Skills",  href: "#skills"},
 ];
 
 export default function Navbar() {
